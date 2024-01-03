b08015c0a9b262d8b1b0d75426ea29060efeaa89
Michael Remy <michaeldremy@gmail.com>
2024-01-02 16:30:09 -0800

    updated App with different way of using react-router and added a vercel.json rewrite file

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,19 @@
 import React from 'react'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './pages/Home'
 import LearnNotes from './pages/LearnNotes'
 import LearnChords from './pages/LearnChords'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/learn-notes',
-    element: <LearnNotes />,
-  },
-  {
-    path: '/learn-chords',
-    element: <LearnChords />,
-  },
-])
-
 function App() {
   return (
-    <>
-      <RouterProvider router={router} />
-    </>
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+        <Route path='/learn-notes' element={<LearnNotes />} />
+        <Route path='/learn-chords' element={<LearnChords />} />
+      </Routes>
+    </Router>
   )
 }
 
